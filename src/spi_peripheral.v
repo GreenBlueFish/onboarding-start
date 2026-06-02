ecbd604b7b0f187e69217b5706f03d0ed188eebc
GreenBlueFish <rowan.leonard76@gmail.com>
2026-06-02 14:58:41 -0400

    changing buffer size in spi_peripheral

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -38,7 +38,7 @@ Two_FF_Synchronizer tff (
 //CPOL = 0 -> idle state low
 //CPHA = 0 -> sample on first edge
 //Rising edge data sample
-reg [15:0] buffer;
+reg [14:0] buffer;
 reg [3:0] counter;
 
 reg [7:0] temp_en_reg_out_7_0;
@@ -61,7 +61,7 @@ always @(posedge SCLK or negedge rst_n) begin
 
     //If the chip select is on and the rst_n is off
     else if((!CS) && (rst_n)) begin
-        buffer <= {buffer[14:0], sync_out};
+        buffer <= {buffer[13:0], sync_out};
         counter <= counter + 1;
         
         if(counter == 4'b1111) begin
